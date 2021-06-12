6c4a994a88833cb11595e59454fd9e2855292107
1NT18IS104 <40169051+1NT18IS104@users.noreply.github.com>
2021-06-12 06:24:39 -0700

    Program 3

---
+++ ProgressCheck.java
@@ -0,0 +1,52 @@
+package my.salchk.pack;
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+	
+public class ProgressCheck {
+
+//MAPPER CODE	
+	   
+public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+		
+	String myString= value.toString();
+	String[] empDetails= myString.split(",");
+	IntWritable awards= new IntWritable(Integer.parseInt(empDetails[3]));
+	if(Integer.parseInt(empDetails[2])==30000) {
+		output.collect(new Text("Total number of awards won by employees with a salary of 30000: "), awards);
+	}
+	}
+}
+
+//REDUCER CODE	
+public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException { 
+	int totalAwards=0;
+	while(values.hasNext()) {
+		totalAwards += values.next().get();
+	}
+	output.collect(key, new IntWritable(totalAwards));
+	}
+}
+	
+//DRIVER CODE
+public static void main(String[] args) throws Exception {
+	JobConf conf = new JobConf(ProgressCheck.class);
+	conf.setJobName("Total Awards with Salary Check");
+	conf.setOutputKeyClass(Text.class);
+	conf.setOutputValueClass(IntWritable.class);
+	conf.setMapperClass(Map.class);
+	conf.setCombinerClass(Reduce.class);
+	conf.setReducerClass(Reduce.class);
+	conf.setInputFormat(TextInputFormat.class);
+	conf.setOutputFormat(TextOutputFormat.class); // hadoop jar jarname classpath inputfolder outputfolder
+	FileInputFormat.setInputPaths(conf, new Path(args[0]));
+	FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+	JobClient.runJob(conf);   
+}
+}
